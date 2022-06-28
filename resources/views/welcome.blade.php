7913498f3678028c3e807345976ca70c76b1368d
Julian Grisales <jgrisales@gmail.com>
2022-06-28 18:39:28 +0200

    Main layout and basic component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://cdn.tailwindcss.com"></script>
-        <title>Laravel</title>
-    </head>
-    <body>
-        <h1>Hallo</h1>
-    </body>
-</html>
+<x-layout>
+    <x-section>
+        Hello World
+    </x-section>
+    <x-section>
+        Hello World
+    </x-section>
+    <x-section>
+        Hello World
+    </x-section>
+</x-layout>
