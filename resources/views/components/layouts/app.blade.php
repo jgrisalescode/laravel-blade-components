e0d25a455d82fbf4ba30042813226e308e745704
Julian Grisales <jgrisales@gmail.com>
2022-06-30 08:11:13 +0200

    Modal Component

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,5 +8,6 @@
     </head>
     <body>
         {{ $slot }}
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,20 @@
-<x-layout>
-    <x-flash type="error">
-        Oops! bad request
-    </x-flash>
-    <br>
-    <x-flash class="mt-10" data-flash="foobar">
-        Greart job!"
-    </x-flash>
-</x-layout>
+<x-layouts.app>
+    <x-modal
+        title="Deactivate Account?"
+        submit-label="Deactivate"
+        {{-- type="warning" --}}
+    >
+        <x-slot name="trigger">
+            <button @click="on = true">Show Modal</button>
+        </x-slot>
+            Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+    </x-modal>
+    <x-modal
+        title="Suscribe"
+    >
+        <x-slot name="trigger">
+            <button @click="on = true">Click me</button>
+        </x-slot>
+            Lorem ipsum.
+    </x-modal>
+</x-layouts.app>
